c1fdedad112b5248fad1b69eab56e7078627b91b
iamsahu <praffulsahu@gmail.com>
2021-06-20 13:22:52 +0530

    First Commit

---
+++ contracts/ERC777Distributor.sol
@@ -0,0 +1,203 @@
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import {
+    ISuperfluid,
+    ISuperToken,
+    SuperAppBase,
+    SuperAppDefinitions
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+//from "https://github.com/superfluid-finance/protocol-monorepo/blob/remix-support/packages/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+import {
+    IInstantDistributionAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
+//from "https://github.com/superfluid-finance/protocol-monorepo/blob/remix-support/packages/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
+
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+
+contract ERC777Distributor is IERC777Recipient,SuperAppBase {
+
+    uint256 totalDontaions;
+
+    uint32 public constant INDEX_ID = 0;
+
+    address owner;
+
+    ISuperToken private _cashToken;
+    ISuperfluid private _host;
+    IInstantDistributionAgreementV1 private _ida;
+
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
+
+    // use callbacks to track approved subscriptions
+    mapping (address => bool) public isSubscribing;
+
+    event DonationReceived(address from, string token, uint256 amount);
+
+    constructor (
+        ISuperToken cashToken,
+        ISuperfluid host,
+        IInstantDistributionAgreementV1 ida) public {
+        
+        _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
+
+        _cashToken = cashToken;
+        _host = host;
+        _ida = ida;
+
+        uint256 configWord =
+            SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP |
+            SuperAppDefinitions.AFTER_AGREEMENT_TERMINATED_NOOP;
+
+        _host.registerApp(configWord);
+
+        _host.callAgreement(
+            _ida,
+            abi.encodeWithSelector(
+                _ida.createIndex.selector,
+                _cashToken,
+                INDEX_ID,
+                new bytes(0) // placeholder ctx
+            ),
+            new bytes(0) // user data
+        );
+    }
+
+    modifier onlyOwner(){
+        require(owner==msg.sender);
+        _;
+    }
+
+    
+
+    function beforeAgreementCreated(
+        ISuperToken superToken,
+        address agreementClass,
+        bytes32 /* agreementId */,
+        bytes calldata /*agreementData*/,
+        bytes calldata /*ctx*/
+    )
+        external view override
+        returns (bytes memory data)
+    {
+        require(superToken == _cashToken, "DRT: Unsupported cash token");
+        require(agreementClass == address(_ida), "DRT: Unsupported agreement");
+        return new bytes(0);
+    }
+
+    function afterAgreementCreated(
+        ISuperToken superToken,
+        address /* agreementClass */,
+        bytes32 agreementId,
+        bytes calldata /*agreementData*/,
+        bytes calldata /*cbdata*/,
+        bytes calldata ctx
+    )
+        external override
+        returns(bytes memory newCtx)
+    {
+        _checkSubscription(superToken, ctx, agreementId);
+        newCtx = ctx;
+    }
+
+    function beforeAgreementUpdated(
+        ISuperToken superToken,
+        address agreementClass,
+        bytes32 /* agreementId */,
+        bytes calldata /*agreementData*/,
+        bytes calldata /*ctx*/
+    )
+        external view override
+        returns (bytes memory data)
+    {
+        require(superToken == _cashToken, "DRT: Unsupported cash token");
+        require(agreementClass == address(_ida), "DRT: Unsupported agreement");
+        return new bytes(0);
+    }
+
+    function afterAgreementUpdated(
+        ISuperToken superToken,
+        address /* agreementClass */,
+        bytes32 agreementId,
+        bytes calldata /*agreementData*/,
+        bytes calldata /*cbdata*/,
+        bytes calldata ctx
+    )
+        external override
+        returns(bytes memory newCtx)
+    {
+        _checkSubscription(superToken, ctx, agreementId);
+        newCtx = ctx;
+    }
+
+    function _checkSubscription(
+        ISuperToken superToken,
+        bytes calldata ctx,
+        bytes32 agreementId
+    )
+        private
+    {
+        ISuperfluid.Context memory context = _host.decodeCtx(ctx);
+        // only interested in the subscription approval callbacks
+        if (context.agreementSelector == IInstantDistributionAgreementV1.approveSubscription.selector) {
+            address publisher;
+            uint32 indexId;
+            bool approved;
+            uint128 units;
+            uint256 pendingDistribution;
+            (publisher, indexId, approved, units, pendingDistribution) =
+                _ida.getSubscriptionByID(superToken, agreementId);
+
+            // sanity checks for testing purpose
+            require(publisher == address(this), "DRT: publisher mismatch");
+            require(indexId == INDEX_ID, "DRT: publisher mismatch");
+
+            if (approved) {
+                isSubscribing[context.msgSender /* subscriber */] = true;
+            }
+        }
+    }
+
+    /// @dev Distribute `amount` of cash among all token holders
+    function distribute(uint256 cashAmount) internal onlyOwner {
+        (uint256 actualCashAmount,) = _ida.calculateDistribution(
+            _cashToken,
+            address(this), INDEX_ID,
+            cashAmount);
+
+        //_cashToken.transferFrom(owner(), address(this), actualCashAmount);
+
+        _host.callAgreement(
+            _ida,
+            abi.encodeWithSelector(
+                _ida.distribute.selector,
+                _cashToken,
+                INDEX_ID,
+                actualCashAmount,
+                new bytes(0) // placeholder ctx
+            ),
+            new bytes(0) // user data
+        );
+    }
+
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external override {
+        //require(msg.sender == address(_token), "Simple777Recipient: Invalid token");
+
+        // do stuff
+        totalDontaions += amount;
+        distribute(amount);
+        emit DonationReceived( from, "tokenName", amount);
+    }   
+
+}
